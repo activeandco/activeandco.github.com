681c281088f83a338bed79e772e28f1735cc1c78
ehoffmann <emmanuel@activeand.co>
2012-11-29 23:32:33 +0100

    Fix mobile css and add jQuery easing plugin

--- assets/themes/ehof/js/activeandco.js
+++ assets/themes/ehof/js/activeandco.js
@@ -1,11 +1,20 @@
 $(function() {
   $("a[rel=popover]")
     .popover({
-      content:'En cours de redaction',
+      content:'En cours de rédaction',
       placement:'top',
-      delay: { show: 500, hide: 100 }
+      delay: { show: 0, hide: 100 }
     })
     .click(function(e) {
-      e.preventDefault()
+      e.preventDefault();
+      //$(this).delay('90').popover('hide');
+    })
+    .mouseleave(function(e) {
+      $(".popover")
+      .delay(50)
+      .animate({ top: '-=200px' }, 500, 'easeInBack')
+      .fadeOut(0, function(){
+        $("a[rel=popver]").delay(550).popover('hide');
+      });
     })
 });
