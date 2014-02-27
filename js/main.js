26d0603669dbaba2c9c0bbb580425208140bb585
ehoffmann <emmanuel@activeand.co>
2014-02-27 17:53:13 +0100

    Css loader and overlay

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 $(function(){
+
+  var overlayDelay = 2000
+  $('.progress-bar').addClass('start')
   // inject contact info via js
   $('address').html('<a href="mailto:hello@activeand.co">hello@activeand.co</a>')
 
@@ -14,22 +17,24 @@ $(function(){
   mywindow = $(window),
   htmlbody = $('html,body');
 
-  //Setup waypoints plugin
-  slide.waypoint(function (direction) {
-    $(this).addClass('reached')
-    //cache the variable of the data-slide attribute associated with each slide
-    dataslide = $(this).attr('data-slide');
-    //If the user scrolls up change the navigation link that has the same data-slide attribute as the slide to active and 
-    //remove the active class from the previous navigation link
-    if (direction === 'down') {
-      $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').prev().removeClass('active');
-    }
-    // else If the user scrolls down change the navigation link that has the same data-slide attribute as the slide to active and 
-    //remove the active class from the next navigation link
-    else {
-      $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').next().removeClass('active');
-    }
-  });
+  setTimeout(function(){
+    //Setup waypoints plugin
+    slide.waypoint(function (direction) {
+      $(this).addClass('reached')
+      //cache the variable of the data-slide attribute associated with each slide
+      dataslide = $(this).attr('data-slide');
+      //If the user scrolls up change the navigation link that has the same data-slide attribute as the slide to active and 
+      //remove the active class from the previous navigation link
+      if (direction === 'down') {
+        $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').prev().removeClass('active');
+      }
+      // else If the user scrolls down change the navigation link that has the same data-slide attribute as the slide to active and 
+      //remove the active class from the next navigation link
+      else {
+        $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').next().removeClass('active');
+      }
+    });
+  }, overlayDelay);
 
   //waypoints doesnt detect the first slide when user scrolls back up to the top so we add this little bit of code, that removes the class 
   //from navigation link slide 2 and adds it to navigation link slide 1.
